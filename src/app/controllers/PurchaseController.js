c19b1831f24070fc7f49901b61fba1ef66531afd
ChilenoSika <jaum97@yahoo.com.br>
2018-12-09 00:32:00 -0200

    Creating templates with handlebars

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,7 +13,8 @@ class PurchaseController {
       from: '"João Medeiros" <jaum97@yahoo.com.br>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra ${purchaseAd.title}`,
-      html: `<p>Teste ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
